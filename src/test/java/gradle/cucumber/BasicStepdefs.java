9039f971500b9a4933328417cd1817979055d46d
DavSanchez <d.vinternatt@gmail.com>
2016-05-24 18:31:33 +0100

    Feature modified and tests added (TODO)

---
+++ src/test/java/gradle/cucumber/BasicStepdefs.java
@@ -0,0 +1,83 @@
+package gradle.cucumber;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class BasicStepdefs {
+    private Pantalla obj;
+    private String resultado;
+
+    @Given("^Estoy en la pantalla principal$")
+    public void estoy_en_la_pantalla_principal() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^pulso botón de dado$")
+    public void pulso_botón_de_dado() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Aparece el resultado de la tirada$")
+    public void aparece_el_resultado_de_la_tirada() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^pulso el botón de lista$")
+    public void pulso_el_botón_de_lista() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Se va a la pantalla de lista de tiradas$")
+    public void se_va_a_la_pantalla_de_lista_de_tiradas() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^Estoy en la pantalla de lista de tiradas$")
+    public void estoy_en_la_pantalla_de_lista_de_tiradas() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^pulso un botón de la lista de tiradas$")
+    public void pulso_un_botón_de_la_lista_de_tiradas() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Aparece el resultado de la tirada de la lista$")
+    public void aparece_el_resultado_de_la_tirada_de_la_lista() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^pulso el boton de añadir elemento$")
+    public void pulso_el_boton_de_añadir_elemento() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Se va a la pantalla de añadir tirada$")
+    public void se_va_a_la_pantalla_de_añadir_tirada() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^pulso el boton de editar elemento$")
+    public void pulso_el_boton_de_editar_elemento() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Se va a la pantalla de modificar tirada$")
+    public void se_va_a_la_pantalla_de_modificar_tirada() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
