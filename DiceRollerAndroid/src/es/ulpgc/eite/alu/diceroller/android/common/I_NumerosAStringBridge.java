10c0c70e8d03f39c2c75a0bde2a09df9252e5c54
DavSanchez <d.vinternatt@gmail.com>
2016-06-28 18:25:26 +0100

    Cleaning and correcting code in Common package...

--- DiceRollerAndroid/src/es/ulpgc/eite/alu/diceroller/android/common/I_NumerosAStringBridge.java
+++ DiceRollerAndroid/src/es/ulpgc/eite/alu/diceroller/android/common/I_NumerosAStringBridge.java
@@ -3,5 +3,4 @@ package es.ulpgc.eite.alu.diceroller.android.common;
 public interface I_NumerosAStringBridge {
     void numberToString(Integer numero);
     String getResultadoTiradaString();
-    void setResultadoTiradaString(String resultadoTiradaString);
 }
